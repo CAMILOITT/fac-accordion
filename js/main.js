91794f2c4f19edda810100792ddd8187d543f879
CAMILOITT <camilocamilo5@outlook.com>
2022-09-29 01:15:42 -0500

    finish style

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-const $button = document.querySelector
+const $questions = document.querySelectorAll('.faq__question');
 
-$button.addEventeListener('click', e =>{
-  
-})
+console.log($questions);
+
+$questions.forEach((question) => {
+  const $answer = question.nextElementSibling;
+  question.addEventListener('click', () => {
+    console.log(question);
+    console.log($answer);
+    question.classList.toggle('faq__question-active');
+    $answer.classList.toggle('faq__answer-hidden');
+  });
+});
