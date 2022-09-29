66fc07fbd66673762716fc21c08ed970a7084ba6
CAMILOITT <camilocamilo5@outlook.com>
2022-09-29 01:39:20 -0500

    add events js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,36 @@ const $questions = document.querySelectorAll('.faq__question');
 
 console.log($questions);
 
+let $ = () => document.querySelector();
+let on = () => addEventListener();
+
 $questions.forEach((question) => {
+
   const $answer = question.nextElementSibling;
   question.addEventListener('click', () => {
     console.log(question);
     console.log($answer);
     question.classList.toggle('faq__question-active');
     $answer.classList.toggle('faq__answer-hidden');
+    console.log('hola');
   });
 });
+
+// $(document).on('click', '.cerrarModal', function () {
+//   $(question).fadeOut();
+//   $('.cerrarModal').attr('class', '');
+// });
+
+// document
+//   .querySelector(document)
+//   .addEventListener('click', '.cerrarModal', function () {
+//     document.querySelector('.Contenedor').fadeOut();
+//     document.querySelector('.cerrarModal').attr('class', '');
+//   });
+
+// document.querySelector('#abrirModal').click(function () {
+//   document.querySelector('.Contenedor').fadeIn();
+//   setTimeout(function () {
+//     document.body.classList.add('cerrarModal');
+//   }, 10);
+// });
