5846ca6079021b7008eb77d9769959d3f0b6bec4
CAMILOITT <camilocamilo5@outlook.com>
2022-09-29 16:15:41 -0500

    add event arrow

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
 const $questions = document.querySelectorAll('.faq__question');
 
 $questions.forEach((question) => {
+  const $imgArrow = question.lastElementChild;
   const $answer = question.nextElementSibling;
-  if (question.classList !== 'faq__question-active') {
-    question.addEventListener('click', () => {
-      question.classList.add('faq__question-active');
-      $answer.classList.remove('faq__answer-hidden');
-    });
-  } else {
-    window.addEventListener('click', () => {
-      question.classList.remove('faq__question-active');
-      $answer.classList.add('faq__answer-hidden');
-    });
-  }
+  question.addEventListener('click', () => {
+    question.classList.toggle('faq__question-active');
+    $answer.classList.toggle('faq__answer-hidden');
+    $imgArrow.classList.toggle('question__img-active');
+  });
 });
