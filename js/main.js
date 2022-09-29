50c570cca522896487b25ffaf8b7a0c42ef70add
CAMILOITT <camilocamilo5@outlook.com>
2022-09-29 14:39:38 -0500

    finish js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,16 @@
 const $questions = document.querySelectorAll('.faq__question');
 
-console.log($questions);
-
-let $ = () => document.querySelector();
-let on = () => addEventListener();
-
 $questions.forEach((question) => {
-
   const $answer = question.nextElementSibling;
-  question.addEventListener('click', () => {
-    console.log(question);
-    console.log($answer);
-    question.classList.toggle('faq__question-active');
-    $answer.classList.toggle('faq__answer-hidden');
-    console.log('hola');
-  });
+  if (question.classList !== 'faq__question-active') {
+    question.addEventListener('click', () => {
+      question.classList.add('faq__question-active');
+      $answer.classList.remove('faq__answer-hidden');
+    });
+  } else {
+    window.addEventListener('click', () => {
+      question.classList.remove('faq__question-active');
+      $answer.classList.add('faq__answer-hidden');
+    });
+  }
 });
-
-// $(document).on('click', '.cerrarModal', function () {
-//   $(question).fadeOut();
-//   $('.cerrarModal').attr('class', '');
-// });
-
-// document
-//   .querySelector(document)
-//   .addEventListener('click', '.cerrarModal', function () {
-//     document.querySelector('.Contenedor').fadeOut();
-//     document.querySelector('.cerrarModal').attr('class', '');
-//   });
-
-// document.querySelector('#abrirModal').click(function () {
-//   document.querySelector('.Contenedor').fadeIn();
-//   setTimeout(function () {
-//     document.body.classList.add('cerrarModal');
-//   }, 10);
-// });
