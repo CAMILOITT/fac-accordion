80c7419b366db09e53a16cc4355a1bc46f827a73
CAMILOITT <camilocamilo5@outlook.com>
2022-10-07 22:23:39 -0500

    modifi js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 const $questions = document.querySelectorAll('.faq__question');
 
-$questions.forEach((question) => {
-  const $imgArrow = question.lastElementChild;
-  const $answer = question.nextElementSibling;
-  question.addEventListener('click', () => {
-    question.classList.toggle('faq__question-active');
-    $answer.classList.toggle('faq__answer-hidden');
-    $imgArrow.classList.toggle('question__img-active');
+function handleToggle(question, $answer, $imgArrow) {
+  question.classList.toggle('faq__question-active');
+  $answer.classList.toggle('faq__answer-hidden');
+  $imgArrow.classList.toggle('question__img-active');
+}
+function handleRemove(question, $answer, $imgArrow) {
+  question.classList.remove('faq__question-active');
+  $answer.classList.add('faq__answer-hidden');
+  $imgArrow.classList.remove('question__img-active');
+}
+window.addEventListener('click', (e) => {
+  $questions.forEach((question) => {
+    const $imgArrow = question.lastElementChild;
+    const $answer = question.nextElementSibling;
+    if (question.contains(e.target)) {
+      handleToggle(question, $answer, $imgArrow);
+    } else {
+      handleRemove(question, $answer, $imgArrow);
+    }
+    console.log('hola');
   });
 });
